e913c8363dfe6f72c60eb18994b04473fab184d0
its-ankit2908 <ap826690@gmail.com>
2021-02-07 18:07:01 +0530

    updation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = new express();
-const port = process.env.port || 8000
+const port = process.env.PORT || 8000
 const path = require('path');
 const hbs = require('hbs');
 
